b1823eba88f8a0240e54f196c97f946feb482a9c
Germano Gascho <germanogascho@gmail.com>
2022-10-20 14:58:02 -0300

    adds theme to the layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import { Box, CssBaseline, Typography } from "@mui/material";
+import { Box, CssBaseline, ThemeProvider, Typography } from "@mui/material";
 import { useState } from "react";
 import { AppDrawer } from "./components/AppDrawer";
 import { AppMain } from "./components/AppMain";
 import { AppToolbar } from "./components/AppToolbar";
+import { theme } from "./theme";
 
 export function App() {
   const [open, setOpen] = useState(false)
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppToolbar onMenuClick={() => setOpen(!open)}/>
-      <AppDrawer open={open}/>
-      <AppMain open={open}>
-        <Typography
-          component="h2"
-          variant="h4"
-        >
-          Content
-        </Typography>
-      </AppMain>
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppToolbar onMenuClick={() => setOpen(!open)}/>
+        <AppDrawer open={open}/>
+        <AppMain open={open}>
+          <Typography
+            component="h2"
+            variant="h4"
+          >
+            Content
+          </Typography>
+        </AppMain>
+      </Box>
+    </ThemeProvider>
   )
 }
