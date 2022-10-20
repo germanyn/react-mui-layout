b1a765f255cdd5683cac3b8cc64b0d301f985705
Germano Gascho <germanogascho@gmail.com>
2022-10-20 17:48:58 -0300

    adds react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { Box, CssBaseline, ThemeProvider, Typography } from "@mui/material";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
 import { useState } from "react";
+import { Outlet } from "react-router-dom";
 import { AppDrawer } from "./components/AppDrawer";
 import { AppMain } from "./components/AppMain";
 import { AppToolbar } from "./components/AppToolbar";
@@ -9,19 +10,14 @@ export function App() {
   const [open, setOpen] = useState(false)
   return (
     <ThemeProvider theme={theme}>
-      <Box sx={{ display: 'flex' }}>
-        <CssBaseline />
-        <AppToolbar onMenuClick={() => setOpen(!open)}/>
-        <AppDrawer open={open} onClose={() => setOpen(false)}/>
-        <AppMain open={open}>
-          <Typography
-            component="h2"
-            variant="h4"
-          >
-            Content
-          </Typography>
-        </AppMain>
-      </Box>
+        <Box sx={{ display: 'flex' }}>
+          <CssBaseline />
+          <AppToolbar onMenuClick={() => setOpen(!open)}/>
+          <AppDrawer open={open} onClose={() => setOpen(false)}/>
+          <AppMain open={open}>
+            <Outlet/>
+          </AppMain>
+        </Box>
     </ThemeProvider>
   )
 }
