2bdb0d2c4f0af00b01c3414dcc8c747762a71f7e
Germano Gascho <germanogascho@gmail.com>
2022-10-20 11:02:22 -0300

    adds toolbar MenuIcon

--- src/components/AppToolbar.tsx
+++ src/components/AppToolbar.tsx
@@ -1,10 +1,17 @@
 import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
-
+import MenuIcon from '@mui/icons-material/Menu'
 
 export function AppToolbar() {
   return (
     <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
       <Toolbar>
+        <IconButton
+          color="inherit"
+          edge="start"
+          sx={{ marginRight: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
         <Typography
           component='h1'
           variant='h6'
