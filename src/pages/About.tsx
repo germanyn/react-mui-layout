cd233ddd44e8fa1f28773a6f0d272c27babc7f88
Germano Gascho <germanogascho@gmail.com>
2022-10-20 17:13:43 -0300

    creates 2 examples pages

---
+++ src/pages/About.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+
+export function About() {
+  return (
+    <Typography
+      component="h2"
+      variant="h4"
+    >
+      About
+    </Typography>
+  )
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+
+export function Home() {
+  return (
+    <Typography
+      component="h2"
+      variant="h4"
+    >
+      Home
+    </Typography>
+  )
+}
